c2a713dbe29140a96d465954656ce3ba4744cdeb
Kasper Garnaes <kasper.garnaes@gmail.com>
2015-01-29 08:53:40 +0100

    Refactor code generation into separate methods.

--- src/Generator.php
+++ src/Generator.php
@@ -28,106 +28,27 @@ class Generator {
 
         $apis = $resource->getApis();
         foreach ($apis as $api) {
-            /** @var ResourceListingApi $a */
-            $serviceGenerator = new ClassGenerator();
-            $name = preg_replace('/(\W*)/', '', $api->getDescription()) . 'Api';
-            $serviceGenerator->setName($name);
-            $serviceGenerator->setExtendedClass('SwaggerApi');
-
-            $uri = UriFactory::factory($api->getPath());
-            $uri->makeRelative($resource->getBasePath());
-            if ($uri->getPath()[0] == '/') {
-                $uri->setPath('.' . $uri->getPath());
-            }
-            $uri->resolve($inputUri->toString());
-
-            $api = new ApiDeclaration(
-                file_get_contents($uri->toString())
-            );
-            foreach ($api->getApis() as $a) {
-                /** @var ApiDeclarationApi $a */
-                foreach ($a->getOperations() as $operation) {
-                    /** @var Operation $operation */
-                    $methodGenerator = new MethodGenerator();
-                    $methodGenerator->setName($operation->getNickname());
-
-                    // Generate the DocBlock
-                    $docBlockGenerator = new DocBlockGenerator();
-                    $docBlockGenerator->setWordWrap(false);
-                    $docBlockGenerator->setShortDescription($operation->getSummary());
-                    $docBlockGenerator->setLongDescription(strip_tags($operation->getNotes()));
-
-                    foreach ($operation->getParameters() as $parameter) {
-                        /** @var Parameter $parameter */
-                        $paramGenerator = new ParameterGenerator($parameter->getName());
-
-                        if (!$parameter->getRequired()) {
-                            $paramGenerator->setDefaultValue(null);
-                        }
-
-                        $tag = new ParamTag(
-                            $parameter->getName(),
-                            $parameter->getDataType(),
-                            $parameter->getDescription()
-                        );
-                        $docBlockGenerator->setTag($tag);
-
-                        $methodGenerator->setParameter($paramGenerator);
-                    }
-                    $methodGenerator->setDocBlock($docBlockGenerator);
-
-                    // Generate the method body
-                    $parameterTypeNames = array(
-                        'path' => array(),
-                        'query' => array(),
-                        'body' => array(),
-                    );
-
-                    foreach ($operation->getParameters() as $parameter) {
-                        /** @var Parameter $parameter */
-                        $parameterTypeNames[$parameter->getParamType()][] = $parameter->getName();
-                    }
-
-                    foreach ($parameterTypeNames as $type => &$names) {
-                        $names = array_map(
-                            function ($name) {
-                                return '$' . $name;
-                            },
-                            $names
-                        );
-                        $names = 'array(' . implode($names, ', ') . ')';
-                    }
-
-                    $requestParams = array(
-                        '"' . $operation->getMethod() . '"',
-                        '"' . $api->getResourcePath() . '"',
-                        $parameterTypeNames['path'],
-                        $parameterTypeNames['query'],
-                        $parameterTypeNames['body'],
-                    );
-                    $body = '$this->request(' . implode($requestParams, ', ') . ');';
-                    $methodGenerator->setBody($body);
-
-
-                    $serviceGenerator->addMethodFromGenerator($methodGenerator);
-                }
-
-            }
+            /** @var ResourceListingApi $api */
+            $service = $this->generateService($api, $resource, $inputUri);
 
             $fileGenerator = new FileGenerator();
             $fileGenerator->setNamespace($options['namespace']);
-
-            if ($a->getDescription()) {
-                $docBlockGenerator = new DocBlockGenerator();
-                $docBlockGenerator->setLongDescription($a->getDescription());
-                $fileGenerator->setDocBlock($docBlockGenerator);
-            }
-
-            $fileGenerator->setFilename($options['outputDir'] . DIRECTORY_SEPARATOR . $name . '.php');
-            $fileGenerator->setClass($serviceGenerator);
+            $fileGenerator->setFilename($options['outputDir'] . DIRECTORY_SEPARATOR . $service->getName() . '.php');
+            $fileGenerator->setClass($service);
             $fileGenerator->write();
         }
 
+        $this->generateSkeleton($options);
+
+    }
+
+    /**
+     * Generate skeleton files needed to use the generated client.
+     *
+     * @param array $options
+     */
+    protected function generateSkeleton(array $options)
+    {
         $files = array(
             'HttpClient.php',
             'SwaggerApi.php',
@@ -139,7 +60,119 @@ class Generator {
             $fileGenerator->setFilename($options['outputDir'] . DIRECTORY_SEPARATOR . $file);
             $fileGenerator->write();
         }
+    }
 
+    /**
+     * Generate
+     *
+     * @param Operation $operation
+     * @param ApiDeclaration $api
+     * @return MethodGenerator
+     */
+    protected function generateMethod(Operation $operation, ApiDeclaration $api)
+    {
+        $methodGenerator = new MethodGenerator();
+        $methodGenerator->setName($operation->getNickname());
+
+        // Generate the DocBlock
+        $docBlockGenerator = new DocBlockGenerator();
+        $docBlockGenerator->setWordWrap(false);
+        $docBlockGenerator->setShortDescription($operation->getSummary());
+        $docBlockGenerator->setLongDescription(strip_tags($operation->getNotes()));
+
+        foreach ($operation->getParameters() as $parameter) {
+            /** @var Parameter $parameter */
+            $paramGenerator = new ParameterGenerator($parameter->getName());
+
+            if (!$parameter->getRequired()) {
+                $paramGenerator->setDefaultValue(null);
+            }
+
+            $tag = new ParamTag(
+                $parameter->getName(),
+                $parameter->getParamType(),
+                $parameter->getDescription()
+            );
+            $docBlockGenerator->setTag($tag);
+
+            $methodGenerator->setParameter($paramGenerator);
+        }
+        $methodGenerator->setDocBlock($docBlockGenerator);
+
+        // Generate the method body
+        $parameterTypeNames = array(
+            'path' => array(),
+            'query' => array(),
+            'body' => array(),
+        );
+
+        foreach ($operation->getParameters() as $parameter) {
+            /** @var Parameter $parameter */
+            $parameterTypeNames[$parameter->getParamType()][] = $parameter->getName();
+        }
+
+        foreach ($parameterTypeNames as $type => &$names) {
+            $names = array_map(
+                function ($name) {
+                    return '$' . $name;
+                },
+                $names
+            );
+            $names = 'array(' . implode($names, ', ') . ')';
+        }
+
+        $requestParams = array(
+            '"' . $operation->getMethod() . '"',
+            '"' . $api->getResourcePath() . '"',
+            $parameterTypeNames['path'],
+            $parameterTypeNames['query'],
+            $parameterTypeNames['body'],
+        );
+        $body = '$this->request(' . implode($requestParams, ', ') . ');';
+        $methodGenerator->setBody($body);
+
+        return $methodGenerator;
+    }
+
+    /**
+     * @param $api
+     * @param $resource
+     * @param $inputUri
+     * @return ClassGenerator
+     */
+    protected function generateService($api, $resource, $inputUri)
+    {
+        $serviceGenerator = new ClassGenerator();
+        $name = preg_replace('/(\W*)/', '', $api->getDescription()) . 'Api';
+        $serviceGenerator->setName($name);
+        $serviceGenerator->setExtendedClass('SwaggerApi');
+
+        $uri = UriFactory::factory($api->getPath());
+        $uri->makeRelative($resource->getBasePath());
+        if ($uri->getPath()[0] == '/') {
+            $uri->setPath('.' . $uri->getPath());
+        }
+        $uri->resolve($inputUri->toString());
+
+        $api = new ApiDeclaration(
+            file_get_contents($uri->toString())
+        );
+        foreach ($api->getApis() as $a) {
+            /** @var ApiDeclarationApi $a */
+            foreach ($a->getOperations() as $operation) {
+                $methodGenerator = $this->generateMethod($operation, $api);
+
+                $serviceGenerator->addMethodFromGenerator($methodGenerator);
+            }
+        }
+
+        if ($api->getDescription()) {
+            $docBlockGenerator = new DocBlockGenerator();
+            $docBlockGenerator->setLongDescription($api->getDescription());
+            $fileGenerator->setDocBlock($docBlockGenerator);
+        }
+
+        return $serviceGenerator;
     }
 
 }
